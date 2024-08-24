65edbd5e35752e5b22df498a0cc00dc1af4a27a8
Tushar Pandao <pandaotushar1@gmail.com>
2024-08-24 15:10:06 +0530

    added the new hollow square Pattern

---
+++ src/main/java/com/booststar/day12_minor_test3/HollowSquarePatternProgram7.java
@@ -0,0 +1,24 @@
+package com.booststar.day12_minor_test3;
+
+public class HollowSquarePatternProgram7 {
+
+    // Program - 7:Hollow Square Pattern
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+            
+            for (int j = 1; j <=5; j++) {
+                
+                if (i==1 || i==5 || j==1|| j==5) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
