5cb855c4a7d46327f99c1b3921f8621ab53b5767
Tushar Pandao <pandaotushar1@gmail.com>
2024-08-27 20:03:26 +0530

    add day 13 Question no 4

---
+++ src/main/java/com/booststar/day13/PrimeNumberChecker.java
@@ -0,0 +1,25 @@
+package com.booststar.day13;
+
+public class PrimeNumberChecker {
+
+    /*
+     * 4. Prime Number Check: Implement a method to determine if a given integer is
+     * a prime number.
+     */
+    public static void main(String[] args) {
+        int num = 16;
+        boolean isPrime = true;
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+
+}
