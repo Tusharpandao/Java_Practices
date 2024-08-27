1710a6ae55a1c71e1e870f6f053356c7ccf8d014
Tushar Pandao <pandaotushar1@gmail.com>
2024-08-27 19:51:09 +0530

    add day 13 Question no 2

---
+++ src/main/java/com/booststar/day13/FibonacciSeries.java
@@ -0,0 +1,24 @@
+package com.booststar.day13;
+
+public class FibonacciSeries {
+
+    /* 2. Fibonacci Series: Create a method to print the first n Fibonacci */
+    public static void main(String[] args) {
+        int n = 10; // Number of Fibonacci numbers to print
+        int first = 0, second = 1;
+        int count = 0;
+
+        System.out.println("First " + n + " numbers in the Fibonacci series:");
+
+        while (count < n) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+
+            count++;
+        }
+    }
+
+}
