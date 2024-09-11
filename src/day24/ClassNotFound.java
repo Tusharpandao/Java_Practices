995fd05c2bc45aa624a0e35a6a5c721b12f53b64
Tushar Pandao <pandaotushar1@gmail.com>
2024-09-11 17:45:36 +0530

    add the update the cart

---
+++ src/day24/ClassNotFound.java
@@ -0,0 +1,11 @@
+package day24;
+public class ClassNotFound {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+
+    }
+}
---
+++ src/day24/IndexOut.java
@@ -0,0 +1,10 @@
+package day24;
+
+public class IndexOut {
+
+    public static void main(String[] args) {
+        System.out.println("hello world tushar");
+
+
+    }
+}
