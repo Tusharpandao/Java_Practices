0ecdd138a9c6b1f934fc081365dc1e91ddd2c588
Tushar Pandao <pandaotushar1@gmail.com>
2024-09-18 19:32:03 +0530

    add the day 4 task

---
+++ src/day4/Demo.java
@@ -0,0 +1,36 @@
+package day4;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        // this is a single line comment
+        System.out.println("hello World");
+
+        /*
+         * this is
+         * a multi-line comment
+         */
+
+        int num = 45;
+        System.out.println("hello World" + num);
+
+        String name = "BOOST";
+        System.err.println(name);
+
+        int number;
+        number = 3;
+        System.out.println(number);
+
+        float myfload = 15.2f;
+        char mychar = 'b';
+        boolean mybool = false;
+        String mytext = "hello";
+
+        System.out.println(mytext);
+        System.err.println(mybool);
+        System.err.println(mychar);
+        System.err.println(myfload);
+
+    }
+
+}
