44c03019fd1485ef7f3bacf96956e1b882002e93
Tushar Pandao <pandaotushar1@gmail.com>
2024-09-23 18:52:37 +0530

    add the day 25 task

---
+++ src/day25/EvenOddChecker.java
@@ -0,0 +1,14 @@
+package day25;
+
+public class EvenOddChecker {
+
+    public static void main(String[] args) {
+        int number = 15; 
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
