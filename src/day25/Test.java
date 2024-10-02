659f447ee5628a6c9eb5683911d6a5d049470b50
Tushar Pandao <pandaotushar1@gmail.com>
2024-10-02 17:51:41 +0530

    new commits

---
+++ src/day25/Test.java
@@ -0,0 +1,22 @@
+package day25;
+import java.util.*;
+public class Test{
+    public static void main(String[] args) {
+        
+        String name="tushar";
+        
+
+        for (int i = name.length()-1;i>=0; i--){
+
+            System.out.print(name.charAt(i) +" ");
+
+        }
+
+    }
+
+
+
+   
+    
+
+}
